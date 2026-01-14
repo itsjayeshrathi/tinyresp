90a5e05fe59d8803b7b4d53235c2f9511a3eaf44
itsjayeshrathi <itsjayeshrathi@gmail.com>
2026-01-14 09:20:33 +0530

    feat: add support for simple error and integer

--- parser.go
+++ parser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 )
 
 type Scanner struct {
@@ -14,6 +15,24 @@ func NewScanner(r io.Reader) *Scanner {
 	return &Scanner{r: bufio.NewReader(r)}
 }
 
+func (s *Scanner) ReadLine() (string, error) {
+	line, err := s.r.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(line) < 2 || line[len(line)-2] != '\r' {
+		return "", fmt.Errorf("protocol error: expected CRLF")
+	}
+
+	return line, nil
+}
+
+func (s *Scanner) ReadBulkString() (int, string, error) {
+	return 0, "", nil
+}
+
 func (s *Scanner) Read() error {
 	t, err := s.r.ReadByte()
 
@@ -22,14 +41,50 @@ func (s *Scanner) Read() error {
 	}
 
 	switch t {
+
 	case '+':
-		line, err := s.r.ReadString('\n')
+		line, err := s.ReadLine()
 		if err != nil {
 			return err
 		}
-		fmt.Println(line)
+		value := line[:len(line)-2]
+		fmt.Printf("SimpleString: %s\n", value)
+
+	case '-':
+		line, err := s.ReadLine()
+		if err != nil {
+			return err
+		}
+		value := line[:len(line)-2]
+
+		fmt.Printf("SimpleError: %s\n", value)
+
+	//:[<+|->]<value>\r\n
+	case ':':
+		line, err := s.ReadLine()
+		if err != nil {
+			return err
+		}
+		value := line[:len(line)-2]
+
+		i, err := strconv.ParseInt(value, 10, 64)
+
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Integer: %d\n", i)
+
+	//$<length>\r\n<data>\r\n
+	case '$':
+	//*<number-of-elements>\r\n<element-1>...<element-n>
+	case '*':
+
 	default:
 		return fmt.Errorf("unsupported RESP type: %q", t)
 	}
 	return nil
+}
+
+func Split(r rune) bool {
+	return r == '\n'
 }
